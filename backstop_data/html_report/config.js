ca82a0dc77644b535b368b011b8a9aa42ca3248a
Daniel Kuroski <daniel.kuroski@gmail.com>
2018-03-08 15:08:30 -0300

    feat(Created backstop tests):

--- backstop_data/html_report/config.js
+++ backstop_data/html_report/config.js
@@ -4,13 +4,91 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/gallo_Button_0_document_0_tablet.png",
-        "test": "../bitmaps_test/20180212-155148/gallo_Button_0_document_0_tablet.png",
+        "test": "../bitmaps_test/20180308-150405/gallo_Button_0_document_0_tablet.png",
         "selector": "document",
         "fileName": "gallo_Button_0_document_0_tablet.png",
         "label": "Button",
         "requireSameDimensions": true,
         "misMatchThreshold": 0,
         "error": "Reference file not found /home/involves/Desktop/violin.css/backstop_data/bitmaps_reference/gallo_Button_0_document_0_tablet.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/gallo_Checkbox_0_document_0_tablet.png",
+        "test": "../bitmaps_test/20180308-150405/gallo_Checkbox_0_document_0_tablet.png",
+        "selector": "document",
+        "fileName": "gallo_Checkbox_0_document_0_tablet.png",
+        "label": "Checkbox",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0,
+        "error": "Reference file not found /home/involves/Desktop/violin.css/backstop_data/bitmaps_reference/gallo_Checkbox_0_document_0_tablet.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/gallo_Content_placeholder_0_document_0_tablet.png",
+        "test": "../bitmaps_test/20180308-150405/gallo_Content_placeholder_0_document_0_tablet.png",
+        "selector": "document",
+        "fileName": "gallo_Content_placeholder_0_document_0_tablet.png",
+        "label": "Content placeholder",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0,
+        "error": "Reference file not found /home/involves/Desktop/violin.css/backstop_data/bitmaps_reference/gallo_Content_placeholder_0_document_0_tablet.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/gallo_Form_text_0_document_0_tablet.png",
+        "test": "../bitmaps_test/20180308-150405/gallo_Form_text_0_document_0_tablet.png",
+        "selector": "document",
+        "fileName": "gallo_Form_text_0_document_0_tablet.png",
+        "label": "Form text",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0,
+        "error": "Reference file not found /home/involves/Desktop/violin.css/backstop_data/bitmaps_reference/gallo_Form_text_0_document_0_tablet.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/gallo_Input_0_document_0_tablet.png",
+        "test": "../bitmaps_test/20180308-150405/gallo_Input_0_document_0_tablet.png",
+        "selector": "document",
+        "fileName": "gallo_Input_0_document_0_tablet.png",
+        "label": "Input",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0,
+        "error": "Reference file not found /home/involves/Desktop/violin.css/backstop_data/bitmaps_reference/gallo_Input_0_document_0_tablet.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/gallo_Input_icon_0_document_0_tablet.png",
+        "test": "../bitmaps_test/20180308-150405/gallo_Input_icon_0_document_0_tablet.png",
+        "selector": "document",
+        "fileName": "gallo_Input_icon_0_document_0_tablet.png",
+        "label": "Input icon",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0,
+        "error": "Reference file not found /home/involves/Desktop/violin.css/backstop_data/bitmaps_reference/gallo_Input_icon_0_document_0_tablet.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/gallo_Label_0_document_0_tablet.png",
+        "test": "../bitmaps_test/20180308-150405/gallo_Label_0_document_0_tablet.png",
+        "selector": "document",
+        "fileName": "gallo_Label_0_document_0_tablet.png",
+        "label": "Label",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0,
+        "error": "Reference file not found /home/involves/Desktop/violin.css/backstop_data/bitmaps_reference/gallo_Label_0_document_0_tablet.png"
       },
       "status": "fail"
     }
