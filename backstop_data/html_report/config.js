2569382f671cf89a5ae76751b88fdd218c8c3e5b
Daniel Kuroski <daniel.kuroski@gmail.com>
2018-05-04 15:10:24 -0300

    style(Changed very-large button max-width):

--- backstop_data/html_report/config.js
+++ backstop_data/html_report/config.js
@@ -4,12 +4,13 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/violin_Button_0_document_0_tablet.png",
-        "test": "../bitmaps_test/20180504-144804/violin_Button_0_document_0_tablet.png",
+        "test": "../bitmaps_test/20180504-150901/violin_Button_0_document_0_tablet.png",
         "selector": "document",
         "fileName": "violin_Button_0_document_0_tablet.png",
         "label": "Button",
         "requireSameDimensions": true,
         "misMatchThreshold": 0,
+        "url": "http://localhost:3000/components/preview/button",
         "error": "Reference file not found /home/involves/dev/violin.css/backstop_data/bitmaps_reference/violin_Button_0_document_0_tablet.png"
       },
       "status": "fail"
@@ -17,12 +18,13 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/violin_Checkbox_0_document_0_tablet.png",
-        "test": "../bitmaps_test/20180504-144804/violin_Checkbox_0_document_0_tablet.png",
+        "test": "../bitmaps_test/20180504-150901/violin_Checkbox_0_document_0_tablet.png",
         "selector": "document",
         "fileName": "violin_Checkbox_0_document_0_tablet.png",
         "label": "Checkbox",
         "requireSameDimensions": true,
         "misMatchThreshold": 0,
+        "url": "http://localhost:3000/components/preview/checkbox",
         "error": "Reference file not found /home/involves/dev/violin.css/backstop_data/bitmaps_reference/violin_Checkbox_0_document_0_tablet.png"
       },
       "status": "fail"
@@ -30,12 +32,13 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/violin_Content_placeholder_0_document_0_tablet.png",
-        "test": "../bitmaps_test/20180504-144804/violin_Content_placeholder_0_document_0_tablet.png",
+        "test": "../bitmaps_test/20180504-150901/violin_Content_placeholder_0_document_0_tablet.png",
         "selector": "document",
         "fileName": "violin_Content_placeholder_0_document_0_tablet.png",
         "label": "Content placeholder",
         "requireSameDimensions": true,
         "misMatchThreshold": 0,
+        "url": "http://localhost:3000/components/preview/content-placeholder",
         "error": "Reference file not found /home/involves/dev/violin.css/backstop_data/bitmaps_reference/violin_Content_placeholder_0_document_0_tablet.png"
       },
       "status": "fail"
@@ -43,12 +46,13 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/violin_Form_text_0_document_0_tablet.png",
-        "test": "../bitmaps_test/20180504-144804/violin_Form_text_0_document_0_tablet.png",
+        "test": "../bitmaps_test/20180504-150901/violin_Form_text_0_document_0_tablet.png",
         "selector": "document",
         "fileName": "violin_Form_text_0_document_0_tablet.png",
         "label": "Form text",
         "requireSameDimensions": true,
         "misMatchThreshold": 0,
+        "url": "http://localhost:3000/components/preview/form-text",
         "error": "Reference file not found /home/involves/dev/violin.css/backstop_data/bitmaps_reference/violin_Form_text_0_document_0_tablet.png"
       },
       "status": "fail"
@@ -56,12 +60,13 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/violin_Input_0_document_0_tablet.png",
-        "test": "../bitmaps_test/20180504-144804/violin_Input_0_document_0_tablet.png",
+        "test": "../bitmaps_test/20180504-150901/violin_Input_0_document_0_tablet.png",
         "selector": "document",
         "fileName": "violin_Input_0_document_0_tablet.png",
         "label": "Input",
         "requireSameDimensions": true,
         "misMatchThreshold": 0,
+        "url": "http://localhost:3000/components/preview/input",
         "error": "Reference file not found /home/involves/dev/violin.css/backstop_data/bitmaps_reference/violin_Input_0_document_0_tablet.png"
       },
       "status": "fail"
@@ -69,12 +74,13 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/violin_Input_icon_0_document_0_tablet.png",
-        "test": "../bitmaps_test/20180504-144804/violin_Input_icon_0_document_0_tablet.png",
+        "test": "../bitmaps_test/20180504-150901/violin_Input_icon_0_document_0_tablet.png",
         "selector": "document",
         "fileName": "violin_Input_icon_0_document_0_tablet.png",
         "label": "Input icon",
         "requireSameDimensions": true,
         "misMatchThreshold": 0,
+        "url": "http://localhost:3000/components/preview/input-icon",
         "error": "Reference file not found /home/involves/dev/violin.css/backstop_data/bitmaps_reference/violin_Input_icon_0_document_0_tablet.png"
       },
       "status": "fail"
@@ -82,12 +88,13 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/violin_Label_0_document_0_tablet.png",
-        "test": "../bitmaps_test/20180504-144804/violin_Label_0_document_0_tablet.png",
+        "test": "../bitmaps_test/20180504-150901/violin_Label_0_document_0_tablet.png",
         "selector": "document",
         "fileName": "violin_Label_0_document_0_tablet.png",
         "label": "Label",
         "requireSameDimensions": true,
         "misMatchThreshold": 0,
+        "url": "http://localhost:3000/components/preview/label",
         "error": "Reference file not found /home/involves/dev/violin.css/backstop_data/bitmaps_reference/violin_Label_0_document_0_tablet.png"
       },
       "status": "fail"
@@ -95,12 +102,13 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/violin_List_0_document_0_tablet.png",
-        "test": "../bitmaps_test/20180504-144804/violin_List_0_document_0_tablet.png",
+        "test": "../bitmaps_test/20180504-150901/violin_List_0_document_0_tablet.png",
         "selector": "document",
         "fileName": "violin_List_0_document_0_tablet.png",
         "label": "List",
         "requireSameDimensions": true,
         "misMatchThreshold": 0,
+        "url": "http://localhost:3000/components/preview/list",
         "error": "Reference file not found /home/involves/dev/violin.css/backstop_data/bitmaps_reference/violin_List_0_document_0_tablet.png"
       },
       "status": "fail"
@@ -108,12 +116,13 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/violin_Form_Field_0_document_0_tablet.png",
-        "test": "../bitmaps_test/20180504-144804/violin_Form_Field_0_document_0_tablet.png",
+        "test": "../bitmaps_test/20180504-150901/violin_Form_Field_0_document_0_tablet.png",
         "selector": "document",
         "fileName": "violin_Form_Field_0_document_0_tablet.png",
         "label": "Form Field",
         "requireSameDimensions": true,
         "misMatchThreshold": 0,
+        "url": "http://localhost:3000/components/preview/form-field",
         "error": "Reference file not found /home/involves/dev/violin.css/backstop_data/bitmaps_reference/violin_Form_Field_0_document_0_tablet.png"
       },
       "status": "fail"
@@ -121,12 +130,13 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/violin_Input_addon_0_document_0_tablet.png",
-        "test": "../bitmaps_test/20180504-144804/violin_Input_addon_0_document_0_tablet.png",
+        "test": "../bitmaps_test/20180504-150901/violin_Input_addon_0_document_0_tablet.png",
         "selector": "document",
         "fileName": "violin_Input_addon_0_document_0_tablet.png",
         "label": "Input addon",
         "requireSameDimensions": true,
         "misMatchThreshold": 0,
+        "url": "http://localhost:3000/components/preview/input-addon",
         "error": "Reference file not found /home/involves/dev/violin.css/backstop_data/bitmaps_reference/violin_Input_addon_0_document_0_tablet.png"
       },
       "status": "fail"
@@ -134,12 +144,13 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/violin_Dropdown_-_default_0_body_0_tablet.png",
-        "test": "../bitmaps_test/20180504-144804/violin_Dropdown_-_default_0_body_0_tablet.png",
+        "test": "../bitmaps_test/20180504-150901/violin_Dropdown_-_default_0_body_0_tablet.png",
         "selector": "body",
         "fileName": "violin_Dropdown_-_default_0_body_0_tablet.png",
         "label": "Dropdown - default",
         "requireSameDimensions": true,
         "misMatchThreshold": 0,
+        "url": "http://localhost:3000/components/preview/dropdown--default",
         "error": "Reference file not found /home/involves/dev/violin.css/backstop_data/bitmaps_reference/violin_Dropdown_-_default_0_body_0_tablet.png"
       },
       "status": "fail"
@@ -147,12 +158,13 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/violin_Dropdown_-_open_0_body_0_tablet.png",
-        "test": "../bitmaps_test/20180504-144804/violin_Dropdown_-_open_0_body_0_tablet.png",
+        "test": "../bitmaps_test/20180504-150901/violin_Dropdown_-_open_0_body_0_tablet.png",
         "selector": "body",
         "fileName": "violin_Dropdown_-_open_0_body_0_tablet.png",
         "label": "Dropdown - open",
         "requireSameDimensions": true,
         "misMatchThreshold": 0,
+        "url": "http://localhost:3000/components/preview/dropdown--open",
         "error": "Reference file not found /home/involves/dev/violin.css/backstop_data/bitmaps_reference/violin_Dropdown_-_open_0_body_0_tablet.png"
       },
       "status": "fail"
@@ -160,12 +172,13 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/violin_Dropdown_-_action_0_body_0_tablet.png",
-        "test": "../bitmaps_test/20180504-144804/violin_Dropdown_-_action_0_body_0_tablet.png",
+        "test": "../bitmaps_test/20180504-150901/violin_Dropdown_-_action_0_body_0_tablet.png",
         "selector": "body",
         "fileName": "violin_Dropdown_-_action_0_body_0_tablet.png",
         "label": "Dropdown - action",
         "requireSameDimensions": true,
         "misMatchThreshold": 0,
+        "url": "http://localhost:3000/components/preview/dropdown--action",
         "error": "Reference file not found /home/involves/dev/violin.css/backstop_data/bitmaps_reference/violin_Dropdown_-_action_0_body_0_tablet.png"
       },
       "status": "fail"
@@ -173,12 +186,13 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/violin_Dropdown_-_counter_0_body_0_tablet.png",
-        "test": "../bitmaps_test/20180504-144804/violin_Dropdown_-_counter_0_body_0_tablet.png",
+        "test": "../bitmaps_test/20180504-150901/violin_Dropdown_-_counter_0_body_0_tablet.png",
         "selector": "body",
         "fileName": "violin_Dropdown_-_counter_0_body_0_tablet.png",
         "label": "Dropdown - counter",
         "requireSameDimensions": true,
         "misMatchThreshold": 0,
+        "url": "http://localhost:3000/components/preview/dropdown--counter",
         "error": "Reference file not found /home/involves/dev/violin.css/backstop_data/bitmaps_reference/violin_Dropdown_-_counter_0_body_0_tablet.png"
       },
       "status": "fail"
@@ -186,12 +200,13 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/violin_Dropdown_-_select_0_body_0_tablet.png",
-        "test": "../bitmaps_test/20180504-144804/violin_Dropdown_-_select_0_body_0_tablet.png",
+        "test": "../bitmaps_test/20180504-150901/violin_Dropdown_-_select_0_body_0_tablet.png",
         "selector": "body",
         "fileName": "violin_Dropdown_-_select_0_body_0_tablet.png",
         "label": "Dropdown - select",
         "requireSameDimensions": true,
         "misMatchThreshold": 0,
+        "url": "http://localhost:3000/components/preview/dropdown--select",
         "error": "Reference file not found /home/involves/dev/violin.css/backstop_data/bitmaps_reference/violin_Dropdown_-_select_0_body_0_tablet.png"
       },
       "status": "fail"
@@ -199,12 +214,13 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/violin_Dropdown_-_multiselect_0_body_0_tablet.png",
-        "test": "../bitmaps_test/20180504-144804/violin_Dropdown_-_multiselect_0_body_0_tablet.png",
+        "test": "../bitmaps_test/20180504-150901/violin_Dropdown_-_multiselect_0_body_0_tablet.png",
         "selector": "body",
         "fileName": "violin_Dropdown_-_multiselect_0_body_0_tablet.png",
         "label": "Dropdown - multiselect",
         "requireSameDimensions": true,
         "misMatchThreshold": 0,
+        "url": "http://localhost:3000/components/preview/dropdown--multiselect",
         "error": "Reference file not found /home/involves/dev/violin.css/backstop_data/bitmaps_reference/violin_Dropdown_-_multiselect_0_body_0_tablet.png"
       },
       "status": "fail"
@@ -212,12 +228,13 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/violin_Radial_Select_0_document_0_tablet.png",
-        "test": "../bitmaps_test/20180504-144804/violin_Radial_Select_0_document_0_tablet.png",
+        "test": "../bitmaps_test/20180504-150901/violin_Radial_Select_0_document_0_tablet.png",
         "selector": "document",
         "fileName": "violin_Radial_Select_0_document_0_tablet.png",
         "label": "Radial Select",
         "requireSameDimensions": true,
         "misMatchThreshold": 0,
+        "url": "http://localhost:3000/components/preview/radial-select",
         "error": "Reference file not found /home/involves/dev/violin.css/backstop_data/bitmaps_reference/violin_Radial_Select_0_document_0_tablet.png"
       },
       "status": "fail"
@@ -225,12 +242,13 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/violin_Text_Link_0_document_0_tablet.png",
-        "test": "../bitmaps_test/20180504-144804/violin_Text_Link_0_document_0_tablet.png",
+        "test": "../bitmaps_test/20180504-150901/violin_Text_Link_0_document_0_tablet.png",
         "selector": "document",
         "fileName": "violin_Text_Link_0_document_0_tablet.png",
         "label": "Text Link",
         "requireSameDimensions": true,
         "misMatchThreshold": 0,
+        "url": "http://localhost:3000/components/preview/text-link",
         "error": "Reference file not found /home/involves/dev/violin.css/backstop_data/bitmaps_reference/violin_Text_Link_0_document_0_tablet.png"
       },
       "status": "fail"
@@ -238,12 +256,13 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/violin_Warning_0_document_0_tablet.png",
-        "test": "../bitmaps_test/20180504-144804/violin_Warning_0_document_0_tablet.png",
+        "test": "../bitmaps_test/20180504-150901/violin_Warning_0_document_0_tablet.png",
         "selector": "document",
         "fileName": "violin_Warning_0_document_0_tablet.png",
         "label": "Warning",
         "requireSameDimensions": true,
         "misMatchThreshold": 0,
+        "url": "http://localhost:3000/components/preview/warning",
         "error": "Reference file not found /home/involves/dev/violin.css/backstop_data/bitmaps_reference/violin_Warning_0_document_0_tablet.png"
       },
       "status": "fail"
@@ -251,15 +270,17 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/violin_InputProgress_0_document_0_tablet.png",
-        "test": "../bitmaps_test/20180504-144804/violin_InputProgress_0_document_0_tablet.png",
+        "test": "../bitmaps_test/20180504-150901/violin_InputProgress_0_document_0_tablet.png",
         "selector": "document",
         "fileName": "violin_InputProgress_0_document_0_tablet.png",
         "label": "InputProgress",
         "requireSameDimensions": true,
         "misMatchThreshold": 0,
+        "url": "http://localhost:3000/components/preview/input-progress",
         "error": "Reference file not found /home/involves/dev/violin.css/backstop_data/bitmaps_reference/violin_InputProgress_0_document_0_tablet.png"
       },
       "status": "fail"
     }
-  ]
+  ],
+  "id": "violin"
 });
