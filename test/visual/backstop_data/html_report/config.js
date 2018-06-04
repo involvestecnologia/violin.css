68f1da4c1e58b66c7703f5c686733694d40e4bdb
onluiz <luiz.alberto1723@gmail.com>
2018-06-04 17:18:37 -0300

    test(fixing tests):

--- test/visual/backstop_data/html_report/config.js
+++ test/visual/backstop_data/html_report/config.js
@@ -4,7 +4,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/violin_Button_0_document_0_desktop.png",
-        "test": "../bitmaps_test/20180604-165620/violin_Button_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20180604-171802/violin_Button_0_document_0_desktop.png",
         "selector": "document",
         "fileName": "violin_Button_0_document_0_desktop.png",
         "label": "Button",
@@ -25,7 +25,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/violin_Checkbox_0_document_0_desktop.png",
-        "test": "../bitmaps_test/20180604-165620/violin_Checkbox_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20180604-171802/violin_Checkbox_0_document_0_desktop.png",
         "selector": "document",
         "fileName": "violin_Checkbox_0_document_0_desktop.png",
         "label": "Checkbox",
@@ -46,7 +46,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/violin_Content_Placeholder_0_document_0_desktop.png",
-        "test": "../bitmaps_test/20180604-165620/violin_Content_Placeholder_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20180604-171802/violin_Content_Placeholder_0_document_0_desktop.png",
         "selector": "document",
         "fileName": "violin_Content_Placeholder_0_document_0_desktop.png",
         "label": "Content Placeholder",
@@ -60,7 +60,7 @@ report({
             "height": 0
           },
           "misMatchPercentage": "0.00",
-          "analysisTime": 114
+          "analysisTime": 70
         }
       },
       "status": "pass"
@@ -68,7 +68,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/violin_Form_Text_0_document_0_desktop.png",
-        "test": "../bitmaps_test/20180604-165620/violin_Form_Text_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20180604-171802/violin_Form_Text_0_document_0_desktop.png",
         "selector": "document",
         "fileName": "violin_Form_Text_0_document_0_desktop.png",
         "label": "Form Text",
@@ -89,7 +89,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/violin_Input__0_document_0_desktop.png",
-        "test": "../bitmaps_test/20180604-165620/violin_Input__0_document_0_desktop.png",
+        "test": "../bitmaps_test/20180604-171802/violin_Input__0_document_0_desktop.png",
         "selector": "document",
         "fileName": "violin_Input__0_document_0_desktop.png",
         "label": "Input ",
@@ -110,7 +110,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/violin_Input_Icon_0_document_0_desktop.png",
-        "test": "../bitmaps_test/20180604-165620/violin_Input_Icon_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20180604-171802/violin_Input_Icon_0_document_0_desktop.png",
         "selector": "document",
         "fileName": "violin_Input_Icon_0_document_0_desktop.png",
         "label": "Input Icon",
@@ -131,7 +131,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/violin_Label_0_document_0_desktop.png",
-        "test": "../bitmaps_test/20180604-165620/violin_Label_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20180604-171802/violin_Label_0_document_0_desktop.png",
         "selector": "document",
         "fileName": "violin_Label_0_document_0_desktop.png",
         "label": "Label",
@@ -152,7 +152,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/violin_List_0_document_0_desktop.png",
-        "test": "../bitmaps_test/20180604-165620/violin_List_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20180604-171802/violin_List_0_document_0_desktop.png",
         "selector": "document",
         "fileName": "violin_List_0_document_0_desktop.png",
         "label": "List",
@@ -173,7 +173,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/violin_Form_Field_0_document_0_desktop.png",
-        "test": "../bitmaps_test/20180604-165620/violin_Form_Field_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20180604-171802/violin_Form_Field_0_document_0_desktop.png",
         "selector": "document",
         "fileName": "violin_Form_Field_0_document_0_desktop.png",
         "label": "Form Field",
@@ -194,7 +194,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/violin_Input_addon_0_document_0_desktop.png",
-        "test": "../bitmaps_test/20180604-165620/violin_Input_addon_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20180604-171802/violin_Input_addon_0_document_0_desktop.png",
         "selector": "document",
         "fileName": "violin_Input_addon_0_document_0_desktop.png",
         "label": "Input addon",
@@ -215,7 +215,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/violin_Dropdown_-_default_0_body_0_desktop.png",
-        "test": "../bitmaps_test/20180604-165620/violin_Dropdown_-_default_0_body_0_desktop.png",
+        "test": "../bitmaps_test/20180604-171802/violin_Dropdown_-_default_0_body_0_desktop.png",
         "selector": "body",
         "fileName": "violin_Dropdown_-_default_0_body_0_desktop.png",
         "label": "Dropdown - default",
@@ -236,7 +236,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/violin_Dropdown_-_open_0_body_0_desktop.png",
-        "test": "../bitmaps_test/20180604-165620/violin_Dropdown_-_open_0_body_0_desktop.png",
+        "test": "../bitmaps_test/20180604-171802/violin_Dropdown_-_open_0_body_0_desktop.png",
         "selector": "body",
         "fileName": "violin_Dropdown_-_open_0_body_0_desktop.png",
         "label": "Dropdown - open",
@@ -257,7 +257,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/violin_Dropdown_-_action_0_body_0_desktop.png",
-        "test": "../bitmaps_test/20180604-165620/violin_Dropdown_-_action_0_body_0_desktop.png",
+        "test": "../bitmaps_test/20180604-171802/violin_Dropdown_-_action_0_body_0_desktop.png",
         "selector": "body",
         "fileName": "violin_Dropdown_-_action_0_body_0_desktop.png",
         "label": "Dropdown - action",
@@ -278,7 +278,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/violin_Dropdown_-_counter_0_body_0_desktop.png",
-        "test": "../bitmaps_test/20180604-165620/violin_Dropdown_-_counter_0_body_0_desktop.png",
+        "test": "../bitmaps_test/20180604-171802/violin_Dropdown_-_counter_0_body_0_desktop.png",
         "selector": "body",
         "fileName": "violin_Dropdown_-_counter_0_body_0_desktop.png",
         "label": "Dropdown - counter",
@@ -299,7 +299,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/violin_Dropdown_-_select_0_body_0_desktop.png",
-        "test": "../bitmaps_test/20180604-165620/violin_Dropdown_-_select_0_body_0_desktop.png",
+        "test": "../bitmaps_test/20180604-171802/violin_Dropdown_-_select_0_body_0_desktop.png",
         "selector": "body",
         "fileName": "violin_Dropdown_-_select_0_body_0_desktop.png",
         "label": "Dropdown - select",
@@ -320,7 +320,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/violin_Dropdown_-_multiselect_0_body_0_desktop.png",
-        "test": "../bitmaps_test/20180604-165620/violin_Dropdown_-_multiselect_0_body_0_desktop.png",
+        "test": "../bitmaps_test/20180604-171802/violin_Dropdown_-_multiselect_0_body_0_desktop.png",
         "selector": "body",
         "fileName": "violin_Dropdown_-_multiselect_0_body_0_desktop.png",
         "label": "Dropdown - multiselect",
@@ -341,7 +341,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/violin_Radial_Select_0_document_0_desktop.png",
-        "test": "../bitmaps_test/20180604-165620/violin_Radial_Select_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20180604-171802/violin_Radial_Select_0_document_0_desktop.png",
         "selector": "document",
         "fileName": "violin_Radial_Select_0_document_0_desktop.png",
         "label": "Radial Select",
@@ -362,7 +362,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/violin_Text_Link_0_document_0_desktop.png",
-        "test": "../bitmaps_test/20180604-165620/violin_Text_Link_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20180604-171802/violin_Text_Link_0_document_0_desktop.png",
         "selector": "document",
         "fileName": "violin_Text_Link_0_document_0_desktop.png",
         "label": "Text Link",
@@ -383,7 +383,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/violin_Warning_0_document_0_desktop.png",
-        "test": "../bitmaps_test/20180604-165620/violin_Warning_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20180604-171802/violin_Warning_0_document_0_desktop.png",
         "selector": "document",
         "fileName": "violin_Warning_0_document_0_desktop.png",
         "label": "Warning",
@@ -404,202 +404,13 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/violin_InputProgress_0_document_0_desktop.png",
-        "test": "../bitmaps_test/20180604-165620/violin_InputProgress_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20180604-171802/violin_InputProgress_0_document_0_desktop.png",
         "selector": "document",
         "fileName": "violin_InputProgress_0_document_0_desktop.png",
         "label": "InputProgress",
         "requireSameDimensions": true,
         "misMatchThreshold": 0,
         "url": "http://localhost:3000/components/preview/input-progress",
-        "diff": {
-          "isSameDimensions": true,
-          "dimensionDifference": {
-            "width": 0,
-            "height": 0
-          },
-          "misMatchPercentage": "0.00"
-        }
-      },
-      "status": "pass"
-    },
-    {
-      "pair": {
-        "reference": "../bitmaps_reference/violin_MenuExpandButton_0_document_0_desktop.png",
-        "test": "../bitmaps_test/20180604-165620/violin_MenuExpandButton_0_document_0_desktop.png",
-        "selector": "document",
-        "fileName": "violin_MenuExpandButton_0_document_0_desktop.png",
-        "label": "MenuExpandButton",
-        "requireSameDimensions": true,
-        "misMatchThreshold": 0,
-        "url": "http://localhost:3000/components/preview/menu-expand-button",
-        "diff": {
-          "isSameDimensions": true,
-          "dimensionDifference": {
-            "width": 0,
-            "height": 0
-          },
-          "misMatchPercentage": "0.00"
-        }
-      },
-      "status": "pass"
-    },
-    {
-      "pair": {
-        "reference": "../bitmaps_reference/violin_MenuDefault_0_document_0_desktop.png",
-        "test": "../bitmaps_test/20180604-165620/violin_MenuDefault_0_document_0_desktop.png",
-        "selector": "document",
-        "fileName": "violin_MenuDefault_0_document_0_desktop.png",
-        "label": "MenuDefault",
-        "requireSameDimensions": true,
-        "misMatchThreshold": 0,
-        "url": "http://localhost:3000/components/preview/menu--default",
-        "diff": {
-          "isSameDimensions": true,
-          "dimensionDifference": {
-            "width": 0,
-            "height": 0
-          },
-          "misMatchPercentage": "0.00"
-        }
-      },
-      "status": "pass"
-    },
-    {
-      "pair": {
-        "reference": "../bitmaps_reference/violin_MenuOpen_0_document_0_desktop.png",
-        "test": "../bitmaps_test/20180604-165620/violin_MenuOpen_0_document_0_desktop.png",
-        "selector": "document",
-        "fileName": "violin_MenuOpen_0_document_0_desktop.png",
-        "label": "MenuOpen",
-        "requireSameDimensions": true,
-        "misMatchThreshold": 0,
-        "url": "http://localhost:3000/components/preview/menu--open",
-        "diff": {
-          "isSameDimensions": true,
-          "dimensionDifference": {
-            "width": 0,
-            "height": 0
-          },
-          "misMatchPercentage": "0.00"
-        }
-      },
-      "status": "pass"
-    },
-    {
-      "pair": {
-        "reference": "../bitmaps_reference/violin_MenuLogoFull_0_document_0_desktop.png",
-        "test": "../bitmaps_test/20180604-165620/violin_MenuLogoFull_0_document_0_desktop.png",
-        "selector": "document",
-        "fileName": "violin_MenuLogoFull_0_document_0_desktop.png",
-        "label": "MenuLogoFull",
-        "requireSameDimensions": true,
-        "misMatchThreshold": 0,
-        "url": "http://localhost:3000/components/preview/menu--logo-full",
-        "diff": {
-          "isSameDimensions": true,
-          "dimensionDifference": {
-            "width": 0,
-            "height": 0
-          },
-          "misMatchPercentage": "0.00"
-        }
-      },
-      "status": "pass"
-    },
-    {
-      "pair": {
-        "reference": "../bitmaps_reference/violin_MenuLogo_0_document_0_desktop.png",
-        "test": "../bitmaps_test/20180604-165620/violin_MenuLogo_0_document_0_desktop.png",
-        "selector": "document",
-        "fileName": "violin_MenuLogo_0_document_0_desktop.png",
-        "label": "MenuLogo",
-        "requireSameDimensions": true,
-        "misMatchThreshold": 0,
-        "url": "http://localhost:3000/components/preview/menu--logo",
-        "diff": {
-          "isSameDimensions": true,
-          "dimensionDifference": {
-            "width": 0,
-            "height": 0
-          },
-          "misMatchPercentage": "0.00"
-        }
-      },
-      "status": "pass"
-    },
-    {
-      "pair": {
-        "reference": "../bitmaps_reference/violin_MenuSearch_0_document_0_desktop.png",
-        "test": "../bitmaps_test/20180604-165620/violin_MenuSearch_0_document_0_desktop.png",
-        "selector": "document",
-        "fileName": "violin_MenuSearch_0_document_0_desktop.png",
-        "label": "MenuSearch",
-        "requireSameDimensions": true,
-        "misMatchThreshold": 0,
-        "url": "http://localhost:3000/components/preview/menu-search",
-        "diff": {
-          "isSameDimensions": true,
-          "dimensionDifference": {
-            "width": 0,
-            "height": 0
-          },
-          "misMatchPercentage": "0.00"
-        }
-      },
-      "status": "pass"
-    },
-    {
-      "pair": {
-        "reference": "../bitmaps_reference/violin_MenuBrandLogo_0_document_0_desktop.png",
-        "test": "../bitmaps_test/20180604-165620/violin_MenuBrandLogo_0_document_0_desktop.png",
-        "selector": "document",
-        "fileName": "violin_MenuBrandLogo_0_document_0_desktop.png",
-        "label": "MenuBrandLogo",
-        "requireSameDimensions": true,
-        "misMatchThreshold": 0,
-        "url": "http://localhost:3000/components/preview/menu-brand-logo",
-        "diff": {
-          "isSameDimensions": true,
-          "dimensionDifference": {
-            "width": 0,
-            "height": 0
-          },
-          "misMatchPercentage": "0.00"
-        }
-      },
-      "status": "pass"
-    },
-    {
-      "pair": {
-        "reference": "../bitmaps_reference/violin_MenuList_0_document_0_desktop.png",
-        "test": "../bitmaps_test/20180604-165620/violin_MenuList_0_document_0_desktop.png",
-        "selector": "document",
-        "fileName": "violin_MenuList_0_document_0_desktop.png",
-        "label": "MenuList",
-        "requireSameDimensions": true,
-        "misMatchThreshold": 0,
-        "url": "http://localhost:3000/components/preview/menu--list",
-        "diff": {
-          "isSameDimensions": true,
-          "dimensionDifference": {
-            "width": 0,
-            "height": 0
-          },
-          "misMatchPercentage": "0.00"
-        }
-      },
-      "status": "pass"
-    },
-    {
-      "pair": {
-        "reference": "../bitmaps_reference/violin_MenuListFull_0_document_0_desktop.png",
-        "test": "../bitmaps_test/20180604-165620/violin_MenuListFull_0_document_0_desktop.png",
-        "selector": "document",
-        "fileName": "violin_MenuListFull_0_document_0_desktop.png",
-        "label": "MenuListFull",
-        "requireSameDimensions": true,
-        "misMatchThreshold": 0,
-        "url": "http://localhost:3000/components/preview/menu--list-full",
         "diff": {
           "isSameDimensions": true,
           "dimensionDifference": {
